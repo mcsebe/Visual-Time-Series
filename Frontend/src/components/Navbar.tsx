0ef5d0fd42867f4165239686397f3214ad69d9d2
mcsebe <sluarteb@gmail.com>
2025-04-23 17:32:23 -0400

    progress in navbar

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import { Container } from "./Container";
 import logoWhite from "../assets/logo_white.jpg";
 import logoDark from "../assets/logo_dark.jpg";
 import { NavItem } from "./NavItem";
+import { useThemeStore } from "../utils/ThemeStore";
 
 const navItems = [
     { name: "Home", path: "/" },
@@ -10,7 +11,9 @@ const navItems = [
 ];
 
 
-export const Navbar = () => { 
+export const Navbar = () => {
+    const { toggleTheme, theme } = useThemeStore();
+
     return (
         <header className="absolute inset-x-0 top-0 z-50 py-3">
             <Container className="flex items-center justify-between">
@@ -32,8 +35,29 @@ export const Navbar = () => {
                         </ul>
                     </div>
 
-                    <div className="min-w-max flex item-center gap-x-3">
-                            <button className={toogleTheme}>Get Started</button>
+                    <div className="min-w-max flex items-center gap-x-3">
+                        <button onClick={toggleTheme}
+                            className="outline-hidden flex relative text-heading-2 rounded-full p-2 lg:p-3 border 
+                            border-box-border cursor-pointer items-center h-12"
+                            >
+                            {theme === "dark" ? (
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                strokeWidth="1.5" stroke="currentColor" className="w-6 h-6" >
+                                    <path strokeLinecap="round" strokeLinejoin="round"
+                                        d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 
+                                        0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 
+                                        21a9.753 9.753 0 009.002-5.998z"/>
+                                </svg>
+                            ) : (
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round"
+                                        d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 
+                                        18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 
+                                        3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"/>
+                                </svg>
+                            )}
+                        </button>
                     </div>
                 </nav>
             </Container>
