1bf5feabe679dc0f324d6db8e65301569046ca1e
mcsebe <sluarteb@gmail.com>
2025-04-24 17:09:02 -0400

    progress in Games page

--- Frontend/src/pages/games/Games.tsx
+++ Frontend/src/pages/games/Games.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import useGames from "../../hook/useGames";
 import { GameCard } from "./components/GameCard";
+import { GameCardSkeleton } from "./components/GameCardSkeleton";
 
 export function Games() {
   const { getGames, response, loading, error } = useGames();
@@ -9,21 +10,21 @@ export function Games() {
     getGames();
   }, []);
 
-  if (loading && !response) {
-    return <div className="flex justify-center items-center h-screen">Loading...</div>;
-  }
-
   return (
     <div className="relative min-h-screen overflow-hidden p-4">
-      <div className="relative z-10 w-full flex flex-wrap justify-center gap-4">
-        {response.map((game) => (
-          <GameCard
-            key={game.app_id}
-            name={game.name}
-            app_id={game.app_id}
-            concurrent_players={game.concurrent_players}
-          />
-        ))}
+      <div className="relative z-10 w-full flex flex-wrap justify-center gap-8">
+        {loading && !response
+          ? Array.from({ length: 100 }).map((_, index) => (
+              <GameCardSkeleton key={index} />
+            ))
+          : response.map((game) => (
+              <GameCard
+                key={game.app_id}
+                name={game.name}
+                app_id={game.app_id}
+                concurrent_players={game.concurrent_players}
+              />
+            ))}
       </div>
     </div>
   );
--- Frontend/src/pages/games/components/GameCard.tsx
+++ Frontend/src/pages/games/components/GameCard.tsx
@@ -1,13 +1,26 @@
 import { GameData } from "../../../types/games";
+import { useState } from "react";
+import ImageNotFound from "../../../assets/NotFound.png";
 
-export const GameCard = ({name, app_id, concurrent_players}: GameData) => {
-    return (
-        <div className="relative max-w-s overflow-hidden rounded-2xl shadow-color-1 group">
-            <img src={`https://cdn.cloudflare.steamstatic.com/steam/apps/${app_id}/header.jpg`} alt={name} className="transition-transform group-hover:scale-110 duration-200" />
-            <div className="absolute inset-0 flex  flex-row items-end bg-gradient-to-t from-black/60 to-transparent">
-                <div className="p-4 text-white">{name}</div>
-                <div className="p-4 text-white">{concurrent_players}</div>
+export const GameCard = ({ name, app_id, concurrent_players }: GameData) => {
+  const [imgSrc, setImgSrc] = useState(`https://cdn.cloudflare.steamstatic.com/steam/apps/${app_id}/header.jpg`);
+
+  return (
+    <div className="relative max-w-115 max-h-55 overflow-hidden rounded-2xl shadow-color-1 group">
+        <a href={`/prediction/${app_id}`}>
+            <img
+                src={imgSrc}
+                onError={() => setImgSrc(ImageNotFound)}
+                alt={name}
+                className="transition-transform group-hover:scale-110 duration-200"
+            />
+            <div className="absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-black/60 to-transparent">
+                <div className="flex justify-between w-full p-3">
+                <div className="text-xl text-white">{name}</div>
+                <div className="text-l text-green-400">{concurrent_players}</div>
+                </div>
             </div>
-        </div>
-    );
-}
+        </a>
+    </div>
+  );
+};
