f505248f3972c3f77f2df107372bff19d2b3c6da
mcsebe <sluarteb@gmail.com>
2025-05-20 17:02:38 -0400

    Layout fix

--- Frontend/src/pages/games/Games.tsx
+++ Frontend/src/pages/games/Games.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import useGames from "../../hook/useGames";
 import { GameCard } from "./components/GameCard";
 import { GameCardSkeleton } from "./components/GameCardSkeleton";
-import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
+import { ErrorMessage } from "../../components/ErrorMessage";
 
 export function Games() {
   const { getGames, response, loading, error } = useGames();
@@ -14,11 +14,7 @@ export function Games() {
   return (
     <div className="relative min-h-screen overflow-hidden p-4 md:px-10 lg:px-15">
       {error ? (
-        <div className="flex flex-col items-center justify-center text-center mt-20 text-gray-600">
-          <ExclamationTriangleIcon className="w-16 h-16 text-red-400 mb-4" />
-          <h2 className="text-4xl font-semibold">Failed to load games</h2>
-          <p className="text-xl mt-2">An error occurred while fetching the information. Please try again later.</p>
-        </div>
+        <ErrorMessage message='Failed to load games'/>
       ) : (
         <div className="relative z-10 w-full flex flex-wrap justify-center gap-8">
           {loading && !response
--- Frontend/src/pages/prediction/prediction.tsx
+++ Frontend/src/pages/prediction/prediction.tsx
@@ -2,6 +2,18 @@ import { PredictionParams } from "./components/predictionParams"
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import useGameHistory from "../../hook/useGameHistory";
+import { ErrorMessage } from "../../components/ErrorMessage";
+import { LineChart } from '@mui/x-charts/LineChart';
+
+
+export const dataset = [
+  { x: 1, y: 2 },
+  { x: 2, y: 5.5 },
+  { x: 3, y: 2 },
+  { x: 5, y: 8.5 },
+  { x: 8, y: 1.5 },
+  { x: 10, y: 5 },
+];
 
 
 export function Prediction() {
@@ -14,12 +26,30 @@ export function Prediction() {
 
   return (
     <div className="relative min-h-screen overflow-hidden p-4 md:px-10 lg:px-15">
-      <h1>{id}</h1>
-      <div className="relative z-10 h-full flex">
-        <div className="flex flex-1 justify-end items-center">
-          <PredictionParams />
+      {error ? (
+        <ErrorMessage message='Failed to load information'/>
+      ) : (
+        <div className="mx-auto max-w-7xl w-full px-10 sm:px-8 md:px-14 lg:px-5      flex flex-col md:flex-row gap-10 lg:gap-12 items-center">
+          <div className="w-full md:w-5/12 lg:w-1/3     border-2 border-gray-200 rounded-lg shadow-md p-4">
+            <div className="w-full relative">
+              <h1>{id}</h1>
+              <PredictionParams />
+            </div>
+          </div>
+
+
+          <div className="w-full md:w-7/12 lg:w-3/4 flex flex-col    border-2 border-gray-200 rounded-lg shadow-md p-4">
+            <LineChart
+              dataset={dataset}
+              xAxis={[{ dataKey: 'x' }]}
+              series={[{ dataKey: 'y' }]}
+              height={300}
+              grid={{ vertical: true, horizontal: true }}
+            />
+          </div>
+
         </div>
-      </div>
+       )}
     </div>
   )
 }
